308279de1b8e2d3952be27968570dffeb15cf835
Adori4nu <adriankowarik@gmail.com>
2023-06-24 14:27:01 +0200

    Jump mechanic blockout

--- Endless_Runer.cpp
+++ Endless_Runer.cpp
@@ -9,6 +9,16 @@ int main()
 
     InitWindow(screenWidth, screenHeight, "Endless Runner");
 
+    const int width = 50;
+    const int height = 80;
+
+    int pos_y{screenHeight - height};
+    int velocity_y{0};
+    const int velocity_of_jump{-10};
+    int gravity{1};
+
+    bool is_in_air{false};
+
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
 
@@ -17,15 +27,35 @@ int main()
     {
         // Update
         //----------------------------------------------------------------------------------
+
+        if (pos_y >= screenHeight - height)
+        {
+            velocity_y = 0;
+            is_in_air = false;
+        }
+        else
+        {
+            velocity_y += gravity;
+        }
+
+        if (IsKeyPressed(KEY_SPACE) && pos_y >= 0 && !is_in_air)
+        {
+            velocity_y += velocity_of_jump;
+            is_in_air = true;
+        }
         
+        pos_y += velocity_y;
+       
         //----------------------------------------------------------------------------------
 
         // Draw
         //----------------------------------------------------------------------------------
         BeginDrawing();
 
-            ClearBackground(RAYWHITE);
+        ClearBackground(RAYWHITE);
 
+        DrawRectangle(screenWidth / 2, pos_y, width, height, RED);
+        
         EndDrawing();
         //----------------------------------------------------------------------------------
     }
