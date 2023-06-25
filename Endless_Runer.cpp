390755d0518da65547bb4d4f0aa585da7e24b441
Adori4nu <adriankowarik@gmail.com>
2023-06-25 14:32:22 +0200

    AnimData update extracted to function

--- Endless_Runer.cpp
+++ Endless_Runer.cpp
@@ -6,6 +6,8 @@ struct AnimData
     Rectangle rect;
     Vector2 pos;
     int frame;
+    float running_time{0};
+    int sprite_length;
 };
 
 struct EnemyNebula
@@ -16,22 +18,43 @@ struct EnemyNebula
     int speed;
 };
 
+const float animation_update_time{1.0f / 12.0f};
+
+AnimData updateAnimData(AnimData& data, float delta_time)
+{
+    data.running_time += delta_time;
+    if (data.running_time >= animation_update_time)
+    {
+        data.running_time = 0.0f;
+
+        data.rect.x = data.frame * data.rect.width;
+        data.frame++;
+        if (data.frame > data.sprite_length)
+        {
+            data.frame = 0;
+        }
+    }
+    return data;
+}
+
 int main()
 {
     // Initialization
     //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    int windowDimensions[2];
+    windowDimensions[0] = 1920;//GetScreenWidth(); // why it's not working properly?
+    windowDimensions[1] = 1080;//GetScreenHeight();
     
     const int gravity{2'500}; // 1'000 pixels per second/second
     const int velocity_of_jump{-1'200}; // 600 pixels per second
     int velocity_y{0};
     bool is_in_air{false};
+    bool double_jump_enable{true};
 
-    InitWindow(screenWidth, screenHeight, "Endless Runner");
+    
+    InitWindow(windowDimensions[0], windowDimensions[1], "Endless Runner");
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
 
-    const float animation_update_time{1.0f / 12.0f};
     
     // player
     Texture2D player_texture = LoadTexture("textures/scarfy.png");
@@ -43,11 +66,12 @@ int main()
         player_texture.height
     };
     player_anim.pos = {
-        screenWidth / 2 - player_anim.rect.width / 2,
-        screenHeight - player_anim.rect.height
+        windowDimensions[0] / 4 - player_anim.rect.width / 2,
+        windowDimensions[1] - player_anim.rect.height
     };
     player_anim.frame = 0;
-    float running_time{};
+    player_anim.sprite_length = 5;
+    // float running_time{};
 
     // enemy
     int enemy_nebula_speed{-600};
@@ -60,30 +84,30 @@ int main()
         {
             {
                 Rectangle{0, 0, single_sprite_width, single_sprite_height},
-                Vector2{screenWidth, screenHeight - single_sprite_height},
-                0
+                Vector2{windowDimensions[0], windowDimensions[1] - single_sprite_height},
+                0, 0, 7
             },
-            screenWidth,
+            windowDimensions[0],
             WHITE,
             enemy_nebula_speed
         },
         {
             {
                 Rectangle{0, single_sprite_height, single_sprite_width, single_sprite_height},
-                Vector2{screenWidth * 2 + 450, screenHeight * 0.5 - single_sprite_height},
-                0
+                Vector2{windowDimensions[0] * 2 + 450, windowDimensions[1] * 0.5 - single_sprite_height},
+                0, 0, 7
             },
-            screenWidth * 2 + 450,
+            windowDimensions[0] * 2 + 450,
             RED,
             enemy_nebula_speed * 1.125
         },
         {
             {
                 Rectangle{0, single_sprite_height, single_sprite_width, single_sprite_height},
-                Vector2{screenWidth * 3, screenHeight - single_sprite_height * 1.25},
-                0
+                Vector2{windowDimensions[0] * 3, windowDimensions[1] - single_sprite_height * 1.25},
+                0, 0, 7
             },
-            screenWidth * 3,
+            windowDimensions[0] * 3,
             BLUE,
             enemy_nebula_speed * 0.65
         }
@@ -98,36 +122,23 @@ int main()
         //----------------------------------------------------------------------------------
         const float delta_time{GetFrameTime()};
 
-
-        running_time += delta_time;
-        if (running_time >= animation_update_time)
-        {
-            running_time = 0.0f;
-            
+        { // sprite animation    
             if (!is_in_air)
             {        
-                player_anim.rect.x = player_anim.frame * player_anim.rect.width;
-                player_anim.frame++;
-                if (player_anim.frame > 5)
-                {
-                    player_anim.frame = 0;
-                }
+                player_anim = updateAnimData(player_anim, delta_time);
             }
             for (auto& nebula : enemy_nebulas)
             {
-                nebula.anim_data.rect.x = nebula.anim_data.frame * nebula.anim_data.rect.width;
-                nebula.anim_data.frame++;
-                if (nebula.anim_data.frame > 7)
-                {
-                    nebula.anim_data.frame = 0;
-                }
+                nebula.anim_data = updateAnimData(nebula.anim_data, delta_time);
             }
         }
         
-        if (player_anim.pos.y >= screenHeight - player_anim.rect.height)
+        // player position logic and update
+        if (player_anim.pos.y >= windowDimensions[1] - player_anim.rect.height)
         {
             velocity_y = 0;
             is_in_air = false;
+            double_jump_enable = true;
         }
         else
         {
@@ -139,16 +150,23 @@ int main()
             velocity_y += velocity_of_jump;
             is_in_air = true;
         }
+        if (IsKeyPressed(KEY_D) && is_in_air && double_jump_enable)
+        {
+            velocity_y += velocity_of_jump * 0.75;
+            double_jump_enable = false;
+        }
         
         player_anim.pos.y += velocity_y * delta_time;
 
+        // respawning enemies off-screen
         for (auto& nebula : enemy_nebulas)
         {
             if (nebula.anim_data.pos.x + nebula.anim_data.rect.width < 0)
             {
-                nebula.anim_data.pos.y = GetRandomValue(screenHeight * 0.5, screenHeight - nebula.anim_data.rect.height);
+                nebula.anim_data.pos.y = GetRandomValue(windowDimensions[1] * 0.5, windowDimensions[1] - nebula.anim_data.rect.height);
                 nebula.anim_data.pos.x = nebula.spawn_distance;
             }
+            // movement from right to left
             nebula.anim_data.pos.x += nebula.speed * delta_time;
         }
        
@@ -161,10 +179,11 @@ int main()
         ClearBackground(RAYWHITE);
 
         DrawTextureRec(player_texture, player_anim.rect, player_anim.pos, WHITE);
-        DrawTextureRec(enemy_nebula_texture, enemy_nebulas[0].anim_data.rect, enemy_nebulas[0].anim_data.pos, enemy_nebulas[0].color);
-        DrawTextureRec(enemy_nebula_texture, enemy_nebulas[1].anim_data.rect, enemy_nebulas[1].anim_data.pos, enemy_nebulas[1].color);
-        DrawTextureRec(enemy_nebula_texture, enemy_nebulas[2].anim_data.rect, enemy_nebulas[2].anim_data.pos, enemy_nebulas[2].color);
-
+        for (auto& nebula : enemy_nebulas)
+        {
+            DrawTextureRec(enemy_nebula_texture, nebula.anim_data.rect, nebula.anim_data.pos, nebula.color);
+        }
+        
         EndDrawing();
         //----------------------------------------------------------------------------------
     }
